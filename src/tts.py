367bbaa9a6050263e9a0359835f08030521a2a5b
Larry-Wendy <your_email_address>
2022-09-26 23:21:28 -0400

    first try

---
+++ src/tts.py
@@ -0,0 +1,13 @@
+import json
+from columbia_student_resource import ColumbiaStudentResource
+
+
+def t1():
+
+    res = ColumbiaStudentResource.get_by_key('d65239a5-d26c-48fc-83e0-9add1cc8e689')
+    print(json.dumps(res, indent=2, default=str))
+
+
+if __name__ == "__main__":
+    print("\n\n Use test_rest.py instead of this file. \n\n")
+    t1()
